153a30165250bb00858a1d6587f6571397ac844e
OgabekDev <ogabekdev@gmail.com>
2023-01-06 09:35:16 +0500

    Initial Commit

---
+++ Task 2/Task_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_2App.swift
+//  Task 2
+//
+//  Created by Ogabek Matyakubov on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
